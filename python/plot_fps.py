a96adb8de1c9a6cc0cf4b97375bbb48dad7a27c9
wehak <hweydahl@gmail.com>
2022-05-10 16:55:09 +0200

    backup

--- python/plot_fps.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import matplotlib.pyplot as plt
-
-filepath = Path("output/log.txt")
-data = filepath.read_text().split(",") # file string to list
-data = [float(x) for x in data if x] # remove empty strings from list
-fig, axs = plt.subplots(1, 1)
-axs.plot(data)
-plt.show()
--- python/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# x = np.random.normal(0, 1, 16).reshape([4,4])
-
-x = np.identity(4)
-y = np.zeros(4*4*3).reshape([4,4,3])
-
-print(x.any())
-print(y.any())
